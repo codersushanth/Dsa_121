2efb7759c150971e0b1f7139d29a3b5db054e1b3
pandas342023 <shriyanprojects2023@gmail.com>
2025-03-22 01:19:40 +0530

    Third Commit

--- Dsa_121/src/basic_array_problems/finding_max_repeated_elements.java
+++ Dsa_121/src/basic_array_problems/finding_max_repeated_elements.java
@@ -1,5 +1,7 @@
 package basic_array_problems;
-
+//The **Boyer-Moore Voting Algorithm** finds the majority element in **O(n) time** using a counter.
+//It keeps a **candidate** and increases the counter for matches. 
+//If the counter reaches **zero**, it picks a new candidate. The final candidate is the majority element. 🚀
 public class finding_max_repeated_elements {
 
 	static int max(int nums[]) {
